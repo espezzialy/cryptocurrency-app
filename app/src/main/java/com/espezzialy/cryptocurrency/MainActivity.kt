df568554f5992047454ad96b96aa44ab6dda36d5
Espezzialy Souza <espezzialy@ciandt.com>
2023-01-31 20:24:58 -0300

    creating repository and adding dependencies

---
+++ app/src/main/java/com/espezzialy/cryptocurrency/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.espezzialy.cryptocurrency
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.espezzialy.cryptocurrency.ui.theme.CryptocurrencyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
